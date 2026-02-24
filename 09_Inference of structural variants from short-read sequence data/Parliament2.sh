f3bbe24f287881a1c720fb815e86c24b7580e12a
ClaretJeanLoup <112483436+ClaretJeanLoup@users.noreply.github.com>
2026-02-24 16:03:20 +0100

    Add files via upload

---
+++ 09_Inference of structural variants from short-read sequence data/Parliament2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH -J Parliament2 
+#SBATCH -A "naiss2023-5-394"
+#SBATCH -p core -n 4
+#SBATCH -t 2:00:00
+
+while getopts b:g:o: flag 
+do
+case "${flag}" in
+                b) BAM=${OPTARG}
+                    	;;
+                g) GEN=${OPTARG}
+                    	;;
+                o) outdir=${OPTARG}
+                    	;;
+                *) echo "Invalid option: -$flag" 
+                	;;
+        esac
+done
+
+cp $BAM $SNIC_TMP
+#cp $BAM.bai $SNIC_TMP #also copy the .bai index
+genfiles="${GEN%.*}" #creates a variable where the name of the ref genome is stored without the extension to copy all dict and fai files
+cp $genfiles* $SNIC_TMP
+
+cd $SNIC_TMP
+bam=$(basename "$BAM")
+ml bioinfo-tools
+ml samtools
+samtools index $bam 
+
+gen=$(basename "$GEN")
+
+export PYTHONNOUSERSITE=True
+export LC_ALL=C
+ls $SNIC_TMP
+
+singularity run -B $SNIC_TMP/:/home/dnanexus/in -B $SNIC_TMP/:/home/dnanexus/out /sw/bioinfo/parliament2/v0.1.11/rackham/bin/parliament2-v0.1.11-0.sif \
+ --bam /home/dnanexus/in/$bam \
+ -r  /home/dnanexus/in/$gen \
+ --prefix=${bam%.bam} \
+ --breakdancer \
+ --manta \
+ --cnvnator \
+ --lumpy \
+ --delly_deletion \
+ --delly_inversion \
+ --delly_duplication \
+ --genotype 
+
+mkdir -p $outdir
+mv sv_caller_results Parliament2_run_${bam%.bam}
+cp -r Parliament2_run_${bam%.bam} $outdir
