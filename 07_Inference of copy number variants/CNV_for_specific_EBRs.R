4e9ea89c39a91eff11cbb6cbb9cc65148b4aeabe
JesperBoman <74507057+JesperBoman@users.noreply.github.com>
2026-05-04 15:00:38 +0200

    Update CNV_for_specific_EBRs.R
    Updated the plot so that both Fission and Fusion EBRs can be plotted at the same time

--- 07_Inference of copy number variants/CNV_for_specific_EBRs.R
+++ 07_Inference of copy number variants/CNV_for_specific_EBRs.R
@@ -10,9 +10,20 @@ CNV.EBR.df<-read.table("CNV.LsinapisSWE_fusion_EBRs.EBRs")
 
 colnames(CNV.EBR.df) <- c("Chromosome", "Start", "End", "Median_ratio", "Copy_number", "ID", "Sampgroup", "Population", "EBR_CHROM", "EBR_START",	"EBR_STOP", "SPECIES", 	"NODE",	"stdBP",	"BUSCO1",	"BUSCO2",	"combID",	"BPid",	"reuse_descendant",	"translocation",	"reciprocal_translocation",	"BRANCH")
 
+CNV.EBR.df$Rearrangement_type <- "Fusions"
+
+CNV.EBR.df2<-read.table("CNV.LsinapisCat_specific_Fissions.v5.EBRs")
+
+colnames(CNV.EBR.df2) <- c("Chromosome", "Start", "End", "Median_ratio", "Copy_number", "ID", "Sampgroup", "Population", "EBR_CHROM", "EBR_START",	"EBR_STOP", "SPECIES", 	"NODE",	"stdBP",	"BUSCO1",	"BUSCO2",	"combID",	"BPid",	"reuse_descendant",	"translocation",	"reciprocal_translocation",	"BRANCH")
+
+CNV.EBR.df2$Rearrangement_type <- "Fissions"
+
+
+cryptics.DF<-CNV.EBR.df[!grepl("morsei", CNV.EBR.df$Population) & !grepl("amurensis", CNV.EBR.df$Population) & !grepl("duponcheli", CNV.EBR.df$Population) & !grepl("lactea", CNV.EBR.df$Population), ]
+
+
 df.CNV.mean <- cryptics.DF %>%
-  #filter(combID != "63-217") %>%
-  group_by(Population, combID, BRANCH, reuse_descendant) %>%
+  group_by(Population, combID, BRANCH, reuse_descendant, Rearrangement_type) %>%
   summarise(
     mean = mean(Median_ratio, na.rm = TRUE),
     sd   = sd(Median_ratio, na.rm = TRUE),
@@ -30,62 +41,80 @@ df.CNV.mean <- df.CNV.mean %>%
   mutate(x_pos = as.numeric(interaction(Population, combID)),
          combID = factor(combID))
 
-# Create rectangles with exact edges
+# Prepare data
+df.CNV.mean <- df.CNV.mean %>%
+  group_by(Rearrangement_type) %>%
+  mutate(x_pos = as.numeric(interaction(Population, combID, drop = TRUE))) %>%
+  ungroup()
+
 rects <- df.CNV.mean %>%
-  group_by(combID) %>%
+  group_by(Rearrangement_type, combID) %>%
   summarise(
     xmin = min(x_pos) - 0.5,
-    xmax = max(x_pos) + 0.5
+    xmax = max(x_pos) + 0.5,
+    .groups = "drop"
   ) %>%
-  mutate(ymin = -Inf,
-         ymax = Inf,
-         fill_color = rep(c("grey", "white"), length.out = n()))
-
-
-
-# Adjust first and last rectangles to exactly match x-axis
-rects[1,]$xmin<-0
-rects$xmax[nrow(rects)] <- max(df.CNV.mean$x_pos) + 0.5
+  group_by(Rearrangement_type) %>%
+  arrange(xmin, .by_group = TRUE) %>%
+  mutate(
+    ymin = -Inf,
+    ymax = Inf,
+    fill_color = rep(c("grey", "white"), length.out = n()),
+    xmin = ifelse(row_number() == 1, min(xmin), xmin),
+    xmax = ifelse(row_number() == n(), max(xmax), xmax)
+  ) %>%
+  ungroup()
 
 # Plot
 ggplot(df.CNV.mean, aes(x = x_pos, y = mean, color = Population)) +
+  
   geom_rect(
     data = rects,
-    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
-    fill = rects$fill_color,
-    inherit.aes = FALSE, alpha=0.5
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = fill_color),
+    inherit.aes = FALSE,
+    alpha = 0.5
   ) +
+  
   geom_point(size = 1, position = position_dodge(width = 0.5)) +
+  
   geom_errorbar(aes(ymin = lower, ymax = upper),
                 width = 0.2,
                 position = position_dodge(width = 0.5)) +
+  
   scale_x_continuous(
     breaks = df.CNV.mean$x_pos,
     labels = interaction(df.CNV.mean$Population, df.CNV.mean$combID),
-    expand = c(0, 0), limits = c(0, NA)
-  ) +
+    expand = c(0, 0)
+  )+
+  facet_wrap(~Rearrangement_type, ncol = 1, scales = "free")+
+  
+  geom_hline(yintercept = 1, lty = 2) +
+  
+  scale_color_manual(
+    name = "Population",
+    values = c(col[5], col2[2], col[1], col[3]),
+    labels = c(
+      expression(italic("Leptidea juvernica") ~ "Sweden"),
+      expression(italic("Leptidea reali") ~ "Catalonia"),
+      expression(italic("Leptidea sinapis") ~ "Catalonia"),
+      expression(italic("Leptidea sinapis") ~ "Sweden")
+    )
+  )+
+  scale_fill_identity() +
+  
   labs(
-    #x = "Fission EBRs",
-    x="Fusion EBRs",
     y = "Haploid copy number",
- #   title = "Mean Median_ratio with 95% CI by Population and combID",
+    x = "EBRs",
     color = "Population"
   ) +
-  geom_hline(yintercept=1, lty=2)+
-
-  scale_color_manual(name="Population", values = c(col[5], col2[2], col[1],  col[3])) +
   
   theme_bw(base_size = 14) +
   theme(
-   #axis.text.x = element_text(angle = 45, hjust = 1),
-    axis.text.x= element_blank(),
-    axis.ticks.x= element_blank(),
-    legend.position = "right"
-  )+
-  theme(
-   # panel.background = element_rect(fill = "white", color = NA), # white background
-    panel.grid.major.x = element_blank(),                        # remove vertical grid
-    panel.grid.minor.x = element_blank(),                        # remove minor vertical grid
-    panel.grid.major.y = element_blank(),         # optional horizontal grid
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = "right",
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank()
   )
